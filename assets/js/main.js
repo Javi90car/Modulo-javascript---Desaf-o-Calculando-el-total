701ec842d8a1c07eeea847afe06bcc9cfa2f1798
Javiera <carmona.javiera90@gmail.com>
2022-12-18 19:51:54 -0300

    Nuevos cambios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
-const precio = 400000;
+ precio = 400000;
 
-const parrafoPrecio = document.querySelector("#precioFijo");
+ parrafoPrecio = document.querySelector("#precioFijo");
 parrafoPrecio.textContent = `Precio: ${precio}`;
 
-const boton = document.querySelector("#btn");
-const color = document.querySelector("#inputColor");
-const cantidad = document.querySelector("#inputCantidad");
-const colorfinal = document.querySelector("#colorCompra");
-const cantidadCompra = document.querySelector("#cantidadCompra");
-const precioCompra = document.querySelector("#precioCompra");
+ boton = document.querySelector("#btn");
+ color = document.querySelector("#inputColor");
+ cantidad = document.querySelector("#inputCantidad");
+ colorfinal = document.querySelector("#colorCompra");
+ cantidadCompra = document.querySelector("#cantidadCompra");
+ precioCompra = document.querySelector("#precioCompra");
 
 boton.addEventListener ("click", () => {
     colorfinal.style.display = "block";
     colorfinal.style.backgroundColor = color.value;
-    cantidadCompra.textContent = `Cantidad seleccionada: ${cantidad.value}`;
-    precioCompra.textContent = `Precio total: $ ${cantidad.value * precio}`; 
+    cantidadCompra.innerHTML = `Cantidad seleccionada: ${cantidad.value}`;
+    precioCompra.innerHTML = `Precio total: $ ${cantidad.value * precio}`; 
 })
